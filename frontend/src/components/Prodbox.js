19b5e980c16d2d55a4e7a319369ca849df0819f5
CGUltimateno <Mohameddeaf65@gmail.com>
2024-09-21 05:41:50 +0300

    Finished Project

--- frontend/src/components/Prodbox.js
+++ frontend/src/components/Prodbox.js
@@ -5,13 +5,23 @@ function Prodbox({ data }) {
         e.currentTarget.classList.toggle('selected');
     };
 
+    const TypeHandler = (data) => {
+        if (data.type === 'DVD') {
+            return data.type = 'Size';
+        } else if (data.type === 'Book') {
+            return data.type = 'Weight';
+        } else {
+            return data.type = 'Dimensions';
+        }
+    };
+
     return (
         <div className="product-wrapper">
             <input type="checkbox" className="delete-checkbox" id={data.sku} onChange={handleCheckboxChange} />
             <p>{data.sku}</p>
             <p>{data.name}</p>
             <p>{data.price} $</p>
-            <p>{data.type}: {data.value + (data.type === 'Size' ? ' MB' : data.type === 'Weight' ? ' KG' : '')}</p>
+            <p>{data.type}: {data.value + (data.type === 'DVD' ? ' MB' : data.type === 'Book' ? ' KG' : '')}</p>
         </div>
     );
 }
